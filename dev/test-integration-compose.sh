1cd0f24be64d8eaa39730b116b5c10fedbaaa569
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2026-08-08 07:43:26 +0900

    style(compose): format integration harness

--- dev/test-integration-compose.sh
+++ dev/test-integration-compose.sh
@@ -5,11 +5,11 @@ files=(-f compose.yaml -f compose.integration.yaml)
 project_name="${COMPOSE_PROJECT_NAME:-acm-it-${RANDOM}-$$}"
 
 cleanup() {
-  docker compose --project-name "${project_name}" "${files[@]}" --profile test down -v --remove-orphans
+  docker compose --project-name "$project_name" "${files[@]}" --profile test down -v --remove-orphans
 }
 trap cleanup EXIT
 
-docker compose --project-name "${project_name}" "${files[@]}" --profile test up \
+docker compose --project-name "$project_name" "${files[@]}" --profile test up \
   --build \
   --abort-on-container-exit \
   --exit-code-from integration-tests
